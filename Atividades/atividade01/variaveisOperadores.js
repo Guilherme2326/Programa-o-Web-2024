ec7ac3d601c6f7374e8b24b6052ce3d508d626dd
Guilherme Assunção <93103953+Guilherme2326@users.noreply.github.com>
2024-03-14 19:00:39 +0000

    Add arithmetic operations using variables

---
+++ Atividades/atividade01/variaveisOperadores.js
@@ -0,0 +1,19 @@
+// Declaração das variáveis numéricas
+let numA = 5;
+let numB = 3;
+
+// Operação de adição
+let resultadoSoma = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB}: ${resultadoSoma}`);
+
+// Operação de subtração
+let resultadoSubtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB}: ${resultadoSubtracao}`);
+
+// Operação de multiplicação
+let resultadoMultiplicacao = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB}: ${resultadoMultiplicacao}`);
+
+// Operação de divisão
+let resultadoDivisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB}: ${resultadoDivisao}`);
