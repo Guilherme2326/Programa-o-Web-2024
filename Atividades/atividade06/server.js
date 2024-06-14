ae501639ab86f53033be34a97225c3371bd343e7
Guilherme Assunção <93103953+Guilherme2326@users.noreply.github.com>
2024-06-14 02:00:28 +0000

    app calc

--- Atividades/atividade06/server.js
+++ Atividades/atividade06/server.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+app.engine('html', bodyParser());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 // Configuração para permitir o uso de arquivos estáticos na pasta views
-app.use(express.static(__dirname + '/views'));
+// app.use(express.static(__dirname + '/views'));
 
 // Configuração para lidar com o corpo das solicitações
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rota para renderizar o formulário
 app.get('/', (req, res) => {
@@ -17,9 +20,14 @@ app.get('/', (req, res) => {
 });
 
 // Rota para receber os dados do formulário e renderizar a página de dados
-app.post('/dados', (req, res) => {
-    const { nome, endereco, telefone, data } = req.body;
-    res.render('dados', { nome, endereco, telefone, data });
+// app.post('/dados', (req, res) => {
+//     const { nome, endereco, telefone, data } = req.body;
+//     res.render('dados', { nome, endereco, telefone, data });
+// });
+
+app.post('/dados', function (req, res) {
+    let dados = req.body;
+    res.render('dados.html', {dados});
 });
 
 // Inicia o servidor
